82eaebf915e2d45abe1795178c78b347c848d6e4
Aliaksandr Mirankou <aliaksandr_mirankou@epam.com>
2023-10-28 14:49:32 +0400

    Task 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://dzn1c90duj.execute-api.us-east-1.amazonaws.com',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://4x1dtkzvqd.execute-api.us-east-1.amazonaws.com',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
